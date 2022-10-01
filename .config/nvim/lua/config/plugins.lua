231ca9da12d7e87c2bd9bf03daf1be04457a5910
Pavel Sobolev <paveloom@riseup.net>
2022-10-01 17:56:01 +0300

    Neovime: add the `nvim-spectre` plugin.

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -12,15 +12,14 @@ if fn.isdirectory(packer_path) == 0 then
   if fn.system({ "git", "-v" }) then
     -- Try to clone the repository
     print("Cloning the `packer` repository...")
-    if
-      fn.system({
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        packer_path,
-      })
+    if fn.system({
+      "git",
+      "clone",
+      "--depth",
+      "1",
+      "https://github.com/wbthomason/packer.nvim",
+      packer_path,
+    })
     then
       -- Add the package path to the runtime path
       o.runtimepath:prepend(packer_path)
@@ -77,6 +76,7 @@ require("config.plugins.nvim-cmp")
 require("config.plugins.nvim-lspconfig")
 require("config.plugins.nvim-notify")
 require("config.plugins.nvim-rooter")
+require("config.plugins.nvim-spectre")
 require("config.plugins.nvim-surround")
 require("config.plugins.nvim-treesitter")
 require("config.plugins.sessions")
---
+++ .config/nvim/lua/config/plugins/nvim-spectre.lua
@@ -0,0 +1,25 @@
+-- Find the enemy and replace them with dark power
+require("packer").use({
+  "nvim-pack/nvim-spectre",
+  requires = { "nvim-lua/plenary.nvim" },
+  after = "lush.nvim",
+  config = function()
+    local name = "spectre"
+    local spectre = require(name)
+    -- Map a keybinding in the normal mode
+    local function nmap(k, e)
+      vim.keymap.set("n", k, e, {
+        noremap = true,
+        silent = true,
+      })
+    end
+
+    -- Setup the plugin
+    spectre.setup({
+      live_update = true,
+      is_insert_mode = true,
+    })
+    -- Setup keybindings
+    nmap("<leader>S", spectre.open)
+  end,
+})
