e19db5836fc77e7f50f3107430f5e814cde700fd
Pavel Sobolev <paveloom@riseup.net>
2022-10-01 23:54:59 +0300

    Neovim: delete the `spellsitter` plugin.

--- .config/nvim/lua/config/plugins/spellsitter.lua
+++
@@ -1,9 +0,0 @@
--- Treesitter powered spellchecker
-require("packer").use({
-  "lewis6991/spellsitter.nvim",
-  requires = { "nvim-treesitter/nvim-treesitter" },
-  after = "nvim-treesitter",
-  config = function()
-    require("spellsitter").setup()
-  end,
-})
