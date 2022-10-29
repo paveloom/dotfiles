2b481657f6bc34b374f33e0d9a5de92882be3a7b
Pavel Sobolev <paveloom@riseup.net>
2022-10-29 12:46:57 +0300

    Neovim: only detect 2 spaces indentation for Lua files.

--- .config/nvim/lua/config/plugins/indent-o-matic.lua
+++ .config/nvim/lua/config/plugins/indent-o-matic.lua
@@ -2,4 +2,12 @@
 require("packer").use({
   "Darazaki/indent-o-matic",
   after = "lush.nvim",
+  config = function()
+    require("indent-o-matic").setup({
+      -- Only detect 2 spaces indentation for Lua files
+      filetype_lua = {
+        standard_widths = { 2 },
+      },
+    })
+  end,
 })
