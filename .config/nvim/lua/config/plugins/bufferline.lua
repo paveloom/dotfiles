3bc2aad0b37d57d1590d33e679ad3ff671c13a31
Pavel Sobolev <paveloom@riseup.net>
2022-12-28 14:09:40 +0300

    Neovim: fix the `bufferline` keybindings.

--- .config/nvim/lua/config/plugins/bufferline.lua
+++ .config/nvim/lua/config/plugins/bufferline.lua
@@ -2,6 +2,7 @@
 return {
   "akinsho/bufferline.nvim",
   dependencies = "nvim-tree/nvim-web-devicons",
+  lazy = true,
   config = function()
     -- Setup the plugin
     require("bufferline").setup({
@@ -17,6 +18,7 @@ return {
         },
         show_buffer_close_icons = false,
         show_close_icon = false,
+        ---@diagnostic disable-next-line: assign-type-mismatch
         separator_style = { "", "" },
         always_show_bufferline = false,
       },
@@ -26,14 +28,32 @@ return {
     local nmap = require("config.utils").nmap
 
     -- Setup keybindings
-    nmap("<leader>.", "<cmd>:tabnew<cr>")
+    nmap("<leader>.", function()
+      require("bufferline")
+      vim.cmd(":tabnew")
+    end)
     nmap("<leader>k", function()
       require("bufferline").cycle(-1)
     end)
     nmap("<leader>;", function()
       require("bufferline").cycle(1)
     end)
-    nmap("<leader>,", "<cmd>:-tabmove<cr>")
-    nmap("<leader>/", "<cmd>:+tabmove<cr>")
+    nmap("<leader>,", function()
+      local tabs = vim.api.nvim_list_tabpages()
+      -- If there is an available tab on the right
+      if tabs[vim.api.nvim_tabpage_get_number(0)] ~= tabs[1] then
+        require("bufferline")
+        vim.cmd(":-tabmove")
+      end
+    end)
+    nmap("<leader>/", function()
+      local tabs = vim.api.nvim_list_tabpages()
+      -- If there is an available tab on the right
+      ---@diagnostic disable-next-line: param-type-mismatch
+      if tabs[vim.api.nvim_tabpage_get_number(0)] ~= vim.fn.get(tabs, -1) then
+        require("bufferline")
+        vim.cmd(":+tabmove")
+      end
+    end)
   end,
 }
