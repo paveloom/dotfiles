0b9326b9e83f81aa06045c210ab33a023384e24e
Pavel Sobolev <paveloom@riseup.net>
2022-12-31 16:31:40 +0300

    Neovim: add the `dressing` plugin.

---
+++ .config/nvim/lua/config/plugins/dressing.lua
@@ -0,0 +1,25 @@
+-- Neovim plugin to improve the default `vim.ui` interfaces
+return {
+  "stevearc/dressing.nvim",
+  lazy = true,
+  dependencies = "nvim-telescope/telescope.nvim",
+  config = function()
+    require("dressing").setup({
+      select = {
+        telescope = require("telescope.themes").get_cursor(),
+      },
+    })
+  end,
+  init = function()
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.ui.select = function(...)
+      require("lazy").load({ plugins = { "dressing.nvim" } })
+      return vim.ui.select(...)
+    end
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.ui.input = function(...)
+      require("lazy").load({ plugins = { "dressing.nvim" } })
+      return vim.ui.input(...)
+    end
+  end,
+}
