74bbf681574c3c77c154108f203f8de52405c6bc
Pavel Sobolev <paveloom@riseup.net>
2023-09-25 17:34:11 +0300

    NixOS: don't force to load the `amdgpu` kernel module.

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -8,15 +8,13 @@
     initrd = {
       availableKernelModules = [
         "ahci"
+        "amdgpu"
         "nvme"
         "rtsx_pci_sdmmc"
         "sd_mod"
         "usb_storage"
         "usbhid"
         "xhci_pci"
-      ];
-      kernelModules = [
-        "amdgpu"
       ];
     };
     kernel.sysctl = {
