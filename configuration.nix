de0cf5c507ca339650fce27e7b9982c5157d334a
Pavel Sobolev <paveloom@riseup.net>
2023-12-07 16:35:47 +0300

    NixOS: enable Whisparr.

--- configuration.nix
+++ configuration.nix
@@ -217,6 +217,12 @@
       enable = true;
       package = pkgs.usbmuxd2;
     };
+    whisparr = {
+      dataDir = config.users.users.paveloom.home + "/.config/whisparr";
+      enable = true;
+      group = "";
+      user = "paveloom";
+    };
     xserver = {
       desktopManager.gnome.enable = true;
       displayManager.gdm.enable = true;
