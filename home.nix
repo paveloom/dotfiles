204ea02dafabda53e708b3a1fed33fd0aa670db9
Pavel Sobolev <paveloomm@gmail.com>
2024-01-03 15:24:37 +0300

    NixOS: add Emacs to favorites.

--- home.nix
+++ home.nix
@@ -69,6 +69,7 @@
         "org.gnome.TextEditor.desktop"
         "org.wezfurlong.wezterm.desktop"
         "code.desktop"
+        "emacsclient.desktop"
         "io.gitlab.news_flash.NewsFlash.desktop"
         "org.nicotine_plus.Nicotine.desktop"
         "org.qbittorrent.qBittorrent.desktop"
