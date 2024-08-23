2b64747672a04430dec8d47fb1859e413f1ea862
Pavel Sobolev <contact@paveloom.dev>
2024-08-23 22:12:31 +0300

    NixOS: remove Console from the favorite apps.

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -69,7 +69,6 @@
         "org.gnome.SystemMonitor.desktop"
         "org.gnome.TextEditor.desktop"
         "org.wezfurlong.wezterm.desktop"
-        "org.gnome.Console.desktop"
         "code.desktop"
         "io.gitlab.news_flash.NewsFlash.desktop"
         "org.qbittorrent.qBittorrent.desktop"
