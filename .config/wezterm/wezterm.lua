00caf80a76da02ee5370f77a0472ba77eb865d20
Pavel Sobolev <contact@paveloom.dev>
2024-08-23 22:11:32 +0300

    WezTerm: switch to the `WebGpu` front-end.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -24,6 +24,7 @@ return {
     -- Disable ligatures
     harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
   }),
+  front_end = "WebGpu";
   hide_tab_bar_if_only_one_tab = true,
   use_fancy_tab_bar = false,
   tab_max_width = 40,
