c37c5e701c0418049435d1684bcb02ad5baefcb6
Pavel Sobolev <contact@paveloom.dev>
2024-09-14 23:36:58 +0300

    NeoVim: enable `nvim-notify` integration for `nlspsettings`.

--- .config/nvim/lua/config/plugins/nlsp-settings.lua
+++ .config/nvim/lua/config/plugins/nlsp-settings.lua
@@ -1,7 +1,14 @@
 -- A plugin for setting Neovim LSP with JSON or YAML files
 return {
   "paveloom/nlsp-settings.nvim",
+  dependencies = {
+    "folke/noice.nvim",
+  },
   config = function()
-    require("nlspsettings").setup({})
+    require("nlspsettings").setup({
+      nvim_notify = {
+        enable = true,
+      },
+    })
   end,
 }
