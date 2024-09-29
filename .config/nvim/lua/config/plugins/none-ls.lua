47c3b91aea4a437a11a44df859a7fa77da5c4b00
Pavel Sobolev <contact@paveloom.dev>
2024-09-29 22:46:09 +0300

    NeoVim: make `none-ls` check if the executable exists.

--- .config/nvim/lua/config/plugins/none-ls.lua
+++ .config/nvim/lua/config/plugins/none-ls.lua
@@ -8,16 +8,22 @@ return {
     "lsp_lines.nvim",
   },
   config = function()
-    local name = "null-ls"
-    local null_ls = require(name)
+    local null_ls = require("null-ls")
     local builtins = null_ls.builtins
     local completion = builtins.completion
     local diagnostics = builtins.diagnostics
     local formatting = builtins.formatting
 
-    null_ls.setup({
-      on_attach = mappings.on_lsp_attach,
-      sources = {
+    local function executable_exists(params)
+      local command = params:get_source().generator.opts.command
+      if command == nil then
+        return false
+      end
+      return vim.fn.executable(command) ~= 0
+    end
+
+    local function get_sources()
+      local _sources = {
         completion.spell,
         diagnostics.fish,
         diagnostics.hadolint,
@@ -25,7 +31,25 @@ return {
         formatting.stylua,
         formatting.xmllint,
         formatting.yamlfmt,
-      },
+      }
+
+      local sources = {}
+
+      for _, source in ipairs(_sources) do
+        table.insert(
+          sources,
+          source.with({
+            runtime_condition = executable_exists,
+          })
+        )
+      end
+
+      return sources
+    end
+
+    null_ls.setup({
+      on_attach = mappings.on_lsp_attach,
+      sources = get_sources(),
     })
   end,
 }
