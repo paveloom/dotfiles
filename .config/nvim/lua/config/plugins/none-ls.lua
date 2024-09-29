92c2169ad1eab82c1e609c9ee057157290184894
Pavel Sobolev <contact@paveloom.dev>
2024-09-29 21:23:03 +0300

    NeoVim: remove runtime conditions from `none-ls` sources.

--- .config/nvim/lua/config/plugins/none-ls.lua
+++ .config/nvim/lua/config/plugins/none-ls.lua
@@ -15,31 +15,15 @@ return {
     local diagnostics = builtins.diagnostics
     local formatting = builtins.formatting
 
-    local function direnv_loaded()
-      return vim.b.direnv_loaded ~= nil
-    end
-
     null_ls.setup({
       on_attach = mappings.on_lsp_attach,
       sources = {
-        completion.spell.with({
-          runtime_condition = direnv_loaded,
-        }),
-        diagnostics.fish.with({
-          runtime_condition = direnv_loaded,
-        }),
-        diagnostics.yamllint.with({
-          runtime_condition = direnv_loaded,
-        }),
-        formatting.stylua.with({
-          runtime_condition = direnv_loaded,
-        }),
-        formatting.xmllint.with({
-          runtime_condition = direnv_loaded,
-        }),
-        formatting.yamlfmt.with({
-          runtime_condition = direnv_loaded,
-        }),
+        completion.spell,
+        diagnostics.fish,
+        diagnostics.yamllint,
+        formatting.stylua,
+        formatting.xmllint,
+        formatting.yamlfmt,
       },
     })
   end,
