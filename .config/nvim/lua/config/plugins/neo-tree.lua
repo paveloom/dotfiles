b7c780e73e648bc034ab7390de906a940fbef5c8
Pavel Sobolev <contact@paveloom.dev>
2024-09-30 22:22:57 +0300

    NeoVim: trash items on `delete` while using Neo-tree.

--- .config/nvim/lua/config/plugins/neo-tree.lua
+++ .config/nvim/lua/config/plugins/neo-tree.lua
@@ -36,9 +36,45 @@ return {
   },
   config = function()
     local neo_tree = require("neo-tree")
-    -- Remove the deprecated commands from v1.x
-    vim.g.neo_tree_remove_legacy_commands = 1
-    -- Set up the plugin
+    local inputs = require("neo-tree.ui.inputs")
+
+    -- Trash the target
+    local function trash(state)
+      local node = state.tree:get_node()
+      if node.type == "message" then
+        return
+      end
+      local _, name = require("neo-tree.utils").split_path(node.path)
+      local msg = string.format("Are you sure you want to trash '%s'?", name)
+      inputs.confirm(msg, function(confirmed)
+        if not confirmed then
+          return
+        end
+        vim.api.nvim_command("silent !trash " .. node.path)
+        require("neo-tree.sources.manager").refresh(state)
+      end)
+    end
+
+    -- Trash the selections (visual mode)
+    local function trash_visual(state, selected_nodes)
+      local paths_to_trash = {}
+      for _, node in ipairs(selected_nodes) do
+        if node.type ~= "message" then
+          table.insert(paths_to_trash, node.path)
+        end
+      end
+      local msg = "Are you sure you want to trash " .. #paths_to_trash .. " items?"
+      inputs.confirm(msg, function(confirmed)
+        if not confirmed then
+          return
+        end
+        for _, path in ipairs(paths_to_trash) do
+          vim.api.nvim_command("silent !trash " .. path)
+        end
+        require("neo-tree.sources.manager").refresh(state)
+      end)
+    end
+
     neo_tree.setup({
       use_default_mappings = false,
       close_if_last_window = true,
@@ -120,6 +156,10 @@ return {
         },
       },
       filesystem = {
+        commands = {
+          delete = trash,
+          delete_visual = trash_visual,
+        },
         components = {
           name = function(config, node, state)
             -- First call the default name component
